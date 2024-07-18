4b80c0e0d1fff8f6b8f84f4a9538918c03cab5ba
Tejas Nampalli <TSNampalli@gmail.com>
2024-07-18 19:53:13 -0400

    finished

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,20 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    result = []
+    nodes_to_visit = [self.root]
+
+    while len(nodes_to_visit) > 0:
+      # Pop 
+      node = nodes_to_visit.pop(0)
+      # append & return
+      if node['id'] == id:
+        result.append(node)
+        return result[0]
+      # Else
+      nodes_to_visit = node['children'] + nodes_to_visit
+
+    #  None
+    return None
+
+
